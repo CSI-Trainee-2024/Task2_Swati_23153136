6d96a641303dd585f2b34f78278041cc461040ec
swati22446 <swati22446@gmail.com>
2024-10-13 18:52:09 +0530

    Workout and Begin button

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-
-
+var timer=false;
+var sec=0;
+var min=0;
+var hr=0;
+function start(){
+    timer=true;
+    let t=document.getElementById("btn1");
+    let x=t.outerHTML=`${hr}:${min}:${sec}`;
+    stopwatch();
+}
+function stopwatch(){
+    if(timer==true){
+        sec=sec+1;
+        if(sec==60){
+            min=min+1;
+            sec=0;
+        }
+        if(min==60){
+            hr=hr+1;
+            min=0;
+        }
+        setTimeout("stopwatch()",1000);
+    }
+}
